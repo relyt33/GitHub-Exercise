8d4fbaa26c6f0a89338cc14c2276ab8bfffdb6a3
Tyler Thames <tylerthames@bellsouth.net>
2023-07-31 20:07:22 -0500

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Tyler Thames on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
